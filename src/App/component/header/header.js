15d7e9dc39fa88a2eb10ac5bb3b9eb8eb3fbed09
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-10-24 23:11:53 +0200

    add: some stuff

--- src/App/component/header/header.js
+++ src/App/component/header/header.js
@@ -17,7 +17,7 @@ export default function Header(){
     return (
         <div className="w-full h-12 bg-blue-500 shadow-xl flex absolute top-0" >
             <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-0 ">
-                <h1 className="text-2xl font-semibold cursor-pointer " onClick={()=>_handlerClickHome()} >GUESS THE FLAG! 🏳️</h1>
+                <h1 className="text-2xl font-semibold cursor-pointer text-white " onClick={()=>_handlerClickHome()} >GUESS THE FLAG! 🏳️</h1>
             </div>
 
             <div className="flex absolute top-1 right-3 bottom-1 bg-white p-2 rounded-full cursor-pointer "  onClick={()=>_handlerClickProfile()} >
