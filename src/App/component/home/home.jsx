1e73c0e2d61a1172f541e367414e72e677f7ac49
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-11-01 12:17:59 +0100

    rem: useNavigate for builf

--- src/App/component/home/home.jsx
+++ src/App/component/home/home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Header from "../header/header";
-import { useNavigate } from "react-router-dom";
 import Footer from "../footer/footer";
 
 
 export default function Home(){
-    const nav = useNavigate()
 
     return (
         <div className="w-screen h-screen bg-slate-700 " >
