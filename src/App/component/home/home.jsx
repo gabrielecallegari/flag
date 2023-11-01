8ab3231ff2dd039b8c538ba5a137f18022bfd19c
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-11-01 12:07:56 +0100

    upd: home

--- src/App/component/home/home.jsx
+++ src/App/component/home/home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../header/header";
+import { useNavigate } from "react-router-dom";
+import Footer from "../footer/footer";
 
 
 export default function Home(){
+    const nav = useNavigate()
 
     return (
         <div className="w-screen h-screen bg-slate-700 " >
             <Header/>
+            <div className="flex w-full h-screen" >
+                
+            </div>
+
+            <Footer />
         </div>
     )
 }
