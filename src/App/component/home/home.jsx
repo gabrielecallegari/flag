96bc9d30f5d66e8a540addc5e0c3c75cf3236354
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-11-02 15:22:09 +0100

    add: motion div

--- src/App/component/home/home.jsx
+++ src/App/component/home/home.jsx
@@ -3,7 +3,7 @@ import Header from "../header/header";
 import Footer from "../footer/footer";
 import { useState } from "react";
 import Notify from "./modal/notify";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -13,6 +13,21 @@ export default function Home(){
 
     const [modal, setModal] = useState(false)
     const navigation = useNavigate()
+
+    const animazione = {
+        hidden: {
+            y: "-50px",
+            opacity: 0
+        },
+        visible: {
+            y: "0",
+            opacity: 1,
+            transition: {
+                duration: 1.5,
+                type: "spring",
+            }
+        },
+    }
 
     useEffect(
         ()=>{
@@ -36,9 +51,9 @@ export default function Home(){
             </AnimatePresence>
             <div className="w-full h-screen px-5 py-3" >
 
-                <div className="w-full h-64 bg-slate-700  rounded-2xl relative shadow-2xl" >
+                <motion.div variants={animazione} initial="hidden" animate="visible" className="w-full h-64 bg-slate-700  rounded-2xl relative shadow-2xl" >
                     <div className="w-full flex justify-center items-center mt-2" >
-                        <h2 className="font-semibold text-white text-2xl" >Higher or Lower</h2>
+                        <h2 className="font-semibold text-white text-2xl mt-2" >Higher or Lower</h2>
                     </div>
 
                     <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center">
@@ -53,11 +68,11 @@ export default function Home(){
                     <div className="absolute bottom-3 left-0 right-0 flex justify-center items-center" >
                         <button className="bg-blue-500 text-2xl text-white px-10 py-2 rounded-xl" onClick={()=>navigation("/hl")} >Gioca ora</button>
                     </div>
-                </div>
+                </motion.div>
 
-                <div className="w-full h-64 bg-slate-700  rounded-2xl relative shadow-2xl mt-10" >
-                    <div className="w-full flex justify-center items-center mt-2" >
-                        <h2 className="font-semibold text-white text-2xl" >Guess the Flag</h2>
+                <motion.div variants={animazione} initial="hidden" animate="visible" className="w-full h-64 bg-slate-700  rounded-2xl relative shadow-2xl mt-10" >
+                    <div className="w-full flex justify-center items-center" >
+                        <h2 className="font-semibold text-white text-2xl mt-3" >Guess the Flag</h2>
                     </div>
 
                     <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center">
@@ -69,7 +84,7 @@ export default function Home(){
                     <div className="absolute bottom-3 left-0 right-0 flex justify-center items-center" >
                         <button className="bg-blue-500 text-2xl text-white px-10 py-2 rounded-xl" onClick={()=>navigation("/flag")} >Gioca ora</button>
                     </div>
-                </div>
+                </motion.div>
                 
             </div>
 
